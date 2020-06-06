ab54d9f10d368501406c8ee4a318595d170ebb61
Chathurika Senani <zenaronline@gmail.com>
2020-06-06 12:06:44 +0530

    Slider added

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,206 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:intro_slider/dot_animation_enum.dart';
+import 'package:intro_slider/intro_slider.dart';
+import 'package:intro_slider/slide_object.dart';
 
-void main() => runApp(MyApp());
+//import 'package:intro_slider_example/home.dart';
+
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Name Generator',
-	  home: RandomWords(),
+      home: IntroScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
-class RandomWordsState extends State<RandomWords> {
 
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
+class IntroScreen extends StatefulWidget {
+  IntroScreen({Key key}) : super(key: key);
 
-	@override
-	Widget build(BuildContext context) {
-		return Scaffold(
-			appBar: AppBar(
-			title: Text('Startup Name Generator'),
-			),
-			body: _buildSuggestions(),
-		);
-	}
-
-	Widget _buildSuggestions() {
-		return ListView.builder(
-			padding: const EdgeInsets.all(16.0),
-			itemBuilder: /*1*/ (context, i) {
-			if (i.isOdd) return Divider(); /*2*/
-
-			final index = i ~/ 2; /*3*/
-			if (index >= _suggestions.length) {
-				_suggestions.addAll(generateWordPairs().take(10)); /*4*/
-			}
-			return _buildRow(_suggestions[index]);
-			});
-	}
-
-	Widget _buildRow(WordPair pair) {
-		return ListTile(
-			title: Text(
-			pair.asPascalCase,
-			style: _biggerFont,
-			),
-		);
-	}
+  @override
+  IntroScreenState createState() => new IntroScreenState();
 }
 
-class RandomWords extends StatefulWidget {
-	@override
-	RandomWordsState createState() => RandomWordsState();
+class IntroScreenState extends State<IntroScreen> {
+  List<Slide> slides = new List();
+
+  Function goToTab;
+
+  @override
+  void initState() {
+    super.initState();
+
+    slides.add(
+      new Slide(
+        title: "SCHOOL",
+        styleTitle: TextStyle(
+            color: Color(0xff3da4ab),
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'RobotoMono'),
+        description:
+            "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.",
+        styleDescription: TextStyle(
+            color: Color(0xfffe9c8f),
+            fontSize: 20.0,
+            fontStyle: FontStyle.italic,
+            fontFamily: 'Raleway'),
+        pathImage: "images/photo_school.png",
+      ),
+    );
+    slides.add(
+      new Slide(
+        title: "MUSEUM",
+        styleTitle: TextStyle(
+            color: Color(0xff3da4ab),
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'RobotoMono'),
+        description:
+            "Ye indulgence unreserved connection alteration appearance",
+        styleDescription: TextStyle(
+            color: Color(0xfffe9c8f),
+            fontSize: 20.0,
+            fontStyle: FontStyle.italic,
+            fontFamily: 'Raleway'),
+        pathImage: "images/photo_museum.png",
+      ),
+    );
+    slides.add(
+      new Slide(
+        title: "COFFEE SHOP",
+        styleTitle: TextStyle(
+            color: Color(0xff3da4ab),
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'RobotoMono'),
+        description:
+            "Much evil soon high in hope do view. Out may few northward believing attempted. Yet timed being songs marry one defer men our. Although finished blessing do of",
+        styleDescription: TextStyle(
+            color: Color(0xfffe9c8f),
+            fontSize: 20.0,
+            fontStyle: FontStyle.italic,
+            fontFamily: 'Raleway'),
+        pathImage: "images/photo_coffee_shop.png",
+      ),
+    );
+  }
+
+  void onDonePress() {
+    // Back to the first tab
+    this.goToTab(0);
+  }
+
+  void onTabChangeCompleted(index) {
+    // Index of current tab is focused
+  }
+
+  Widget renderNextBtn() {
+    return Icon(
+      Icons.navigate_next,
+      color: Color(0xffffcc5c),
+      size: 35.0,
+    );
+  }
+
+  Widget renderDoneBtn() {
+    return Icon(
+      Icons.done,
+      color: Color(0xffffcc5c),
+    );
+  }
+
+  Widget renderSkipBtn() {
+    return Icon(
+      Icons.skip_next,
+      color: Color(0xffffcc5c),
+    );
+  }
+
+  List<Widget> renderListCustomTabs() {
+    List<Widget> tabs = new List();
+    for (int i = 0; i < slides.length; i++) {
+      Slide currentSlide = slides[i];
+      tabs.add(Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Container(
+          margin: EdgeInsets.only(bottom: 60.0, top: 60.0),
+          child: ListView(
+            children: <Widget>[
+              GestureDetector(
+                  child: Image.asset(
+                currentSlide.pathImage,
+                width: 200.0,
+                height: 200.0,
+                fit: BoxFit.contain,
+              )),
+              Container(
+                child: Text(
+                  currentSlide.title,
+                  style: currentSlide.styleTitle,
+                  textAlign: TextAlign.center,
+                ),
+                margin: EdgeInsets.only(top: 20.0),
+              ),
+              Container(
+                child: Text(
+                  currentSlide.description,
+                  style: currentSlide.styleDescription,
+                  textAlign: TextAlign.center,
+                  maxLines: 5,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                margin: EdgeInsets.only(top: 20.0),
+              ),
+            ],
+          ),
+        ),
+      ));
+    }
+    return tabs;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new IntroSlider(
+      // List slides
+      slides: this.slides,
+
+      // Skip button
+      renderSkipBtn: this.renderSkipBtn(),
+      colorSkipBtn: Color(0x33ffcc5c),
+      highlightColorSkipBtn: Color(0xffffcc5c),
+
+      // Next button
+      renderNextBtn: this.renderNextBtn(),
+
+      // Done button
+      renderDoneBtn: this.renderDoneBtn(),
+      onDonePress: this.onDonePress,
+      colorDoneBtn: Color(0x33ffcc5c),
+      highlightColorDoneBtn: Color(0xffffcc5c),
+
+      // Dot indicator
+      colorDot: Color(0xffffcc5c),
+      sizeDot: 13.0,
+      typeDotAnimation: dotSliderAnimation.SIZE_TRANSITION,
+
+      // Tabs
+      listCustomTabs: this.renderListCustomTabs(),
+      backgroundColorAllSlides: Colors.white,
+      refFuncGoToTab: (refFunc) {
+        this.goToTab = refFunc;
+      },
+
+      // Show or hide status bar
+      shouldHideStatusBar: true,
+
+      // On tab change completed
+      onTabChangeCompleted: this.onTabChangeCompleted,
+    );
+  }
 }
